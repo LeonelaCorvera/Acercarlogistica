7068cfc7a6c6c4bb67a2647cee504b8213f822a6
leonelacorvera90@gmail.com <thiagot137@alu.bue.edu.ar>
2020-10-26 00:52:20 -0300

    comit nuevo hoy

--- funciones/config.php
+++ funciones/config.php
@@ -1,11 +1,16 @@
 <?php
+	/*
 	//database CONSTANTES
-	define('SQL_SERVER','127.0.0.1');
-
+	define('SQL_SERVER','localhost');
 	define('SQL_USER','root');
 	define('SQL_PASS','');
 	//define('SQL_DB','friendface2');
 	define('SQL_DB','acercarlogistca');
+*/
 
-	//jdbc:mysql://localhost:2222/?user=root
+	define('SQL_SERVER','remotemysql.com');
+	define('SQL_USER','AU1sul0gxE');
+	define('SQL_PASS','TDTLZgtXMg');
+	//define('SQL_DB','friendface2');
+	define('SQL_DB','AU1sul0gxE');
 ?>
